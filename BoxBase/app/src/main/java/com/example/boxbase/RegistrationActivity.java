10ada4474f832705cf47fe89724329759e9fd346
fxsth <fxsth@pm.me>
2020-06-19 19:14:14 +0200

    Aus dem Registrationscreen kommt man nun ins MainMenu

--- BoxBase/app/src/main/java/com/example/boxbase/RegistrationActivity.java
+++ BoxBase/app/src/main/java/com/example/boxbase/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.boxbase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,14 @@ public class RegistrationActivity extends AppCompatActivity {
         final Button createAccountButton = findViewById(R.id.button_create_account);
         final Button discardButton = findViewById(R.id.button_discard);
 
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(RegistrationActivity.this, MainMenu.class);
+                RegistrationActivity.this.startActivity(mainIntent);
+                finish();
+            }
+        });
 
         discardButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +32,6 @@ public class RegistrationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 }
