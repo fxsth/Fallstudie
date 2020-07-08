29c380861c36076d5e138c7d87b68e41597b4373
LukasRogalsky <64218363+LukasRogalsky@users.noreply.github.com>
2020-07-08 19:41:47 +0200

    "today" bei der Zeitraumauswahl hinzugefügt

--- BoxBase/app/src/main/java/com/example/boxbase/ui/RedirectActivity.java
+++ BoxBase/app/src/main/java/com/example/boxbase/ui/RedirectActivity.java
@@ -20,7 +20,7 @@ public class RedirectActivity extends AppCompatActivity implements AdapterView.O
 
 
     // Declaration for dropdown menus
-    private static final String[] paths_day_selection = {"tomorrow", "in 2 days", "in 3 days", "in 4 days", "in 5 days"};
+    private static final String[] paths_day_selection = {"today", "tomorrow", "in 2 days", "in 3 days", "in 4 days", "in 5 days"};
     private static final String[] paths_time_selection_from = {"6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"};
     private static final String[] paths_time_selection_to = {"7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm"};
 
